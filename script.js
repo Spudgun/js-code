3bf528f5eba7849b9e7c1f5eed1ad94587fa4adc
Spudgun <jared.rj.white@gmail.com>
2024-11-23 13:13:58 +0000

    feat: first lesson

---
+++ script.js
@@ -0,0 +1,41 @@
+//printing to console variables.
+let firstname = "John"
+let surname = "Sparks"
+
+console.log(firstname);
+console.log(surname);
+
+
+//reassignment of already declared variable
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+//showing that constant variables don't change with reassignment -> should throw console error.
+const pi = 3.14;
+//pi = 10;
+
+console.log(pi);
+
+// evulating mathematical expressions
+
+console.log((3+2) - 76 * (1+1));
+
+//
+
+console.log("--Asignment from this module---");
+
+console.log(23 + 97);
+
+console.log(1+2+3+4+5+6);
+
+console.log((4+6+9)/72);
+
+let max = 57;
+
+let actual = max - 13;
+
+let percentage = actual / max;
