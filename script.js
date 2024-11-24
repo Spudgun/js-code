e2ef7a2cfb3080e87b413ccfc58c8b70f4abda0e
Spudgun <jared.rj.white@gmail.com>
2024-11-24 18:09:18 +0000

    assignment: for loop with conditional if statements for reminader operator

--- script.js
+++ script.js
@@ -1,41 +1,13 @@
-//printing to console variables.
-let firstname = "John"
-let surname = "Sparks"
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
-console.log(firstname);
-console.log(surname);
-
-
-//reassignment of already declared variable
-let age = 11;
-console.log(age);
-
-age = 54;
-
-console.log(age);
-
-//showing that constant variables don't change with reassignment -> should throw console error.
-const pi = 3.14;
-//pi = 10;
-
-console.log(pi);
-
-// evulating mathematical expressions
-
-console.log((3+2) - 76 * (1+1));
-
-//
-
-console.log("--Asignment from this module---");
-
-console.log(23 + 97);
-
-console.log(1+2+3+4+5+6);
-
-console.log((4+6+9)/72);
-
-let max = 57;
-
-let actual = max - 13;
-
-let percentage = actual / max;
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("Fizzbang");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else if (i % 5 === 0){
+        console.log("bang");
+    } else {
+        console.log(i);
+    }
+}
